c980430bbf0cdc71a29100bec01a19c0eb192469
wooshanghyeon <woosh9912@naver.com>
2023-09-26 11:16:24 +0900

    Combine Subscription

--- Combine/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Combine/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,23 @@
 import Foundation
 import Combine
 
-
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
+let subscription1 = subject
+    .print("[Debug]")
+    .sink { value in
+    print("subscriotion value : \(value)")
+}
+
+subject.send("first")
+subject.send("second")
+subject.send("third")
+//subject.send(completion: .finished)
+subscription1.cancel()
+subject.send("final")
+
+
 
 
 //: [Next](@next)
