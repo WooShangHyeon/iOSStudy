c2428002799a567bb4120435906437a312ed23a1
wooshanghyeon <woosh9912@naver.com>
2023-09-26 11:29:11 +0900

    Combine Published

--- Combine/Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Combine/Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,7 +4,22 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "woo"
+    var age: Int = 25
+}
 
+final class Label {
+    var text: String = "hello"
+}
 
+let label = Label()
+let vm = SomeViewModel()
+
+print("label text : \(label.text)")
+vm.$name.assign(to: \.text, on: label)
+print("label text : \(label.text)")
+vm.name = "you"
+print("label text : \(label.text)")
 
 //: [Next](@next)
