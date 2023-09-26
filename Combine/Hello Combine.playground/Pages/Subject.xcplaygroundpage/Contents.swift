778cedb0c80469f2d659d1b44adab061ba21f28b
wooshanghyeon <woosh9912@naver.com>
2023-09-26 10:57:57 +0900

    Combine Subject

--- Combine/Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Combine/Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -2,13 +2,25 @@ import Foundation
 import Combine
 
 // PassthroughSubject
-
+let relay = PassthroughSubject<String, Never>()
+let subscription1 = relay.sink { value in
+    print("subscription1 : \(value)")
+}
+//
+//relay.send("Hello")
+//relay.send("world!")
 
 
 
 // CurrentValueSubject
 
+let variable = CurrentValueSubject<String, Never>("initial")
+let subscription2 = variable.sink { value in
+    print("subscription2 : \(value)")
+}
 
+//variable.send("\(variable.value) + second value")
 
-
-
+let publisher = ["woo","sang","hyeon"].publisher
+publisher.subscribe(relay)
+publisher.subscribe(variable)
