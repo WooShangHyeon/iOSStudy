86abe5ad644746c670e8a8e6eecda92c2fb767e7
wooshanghyeon <woosh9912@naver.com>
2023-09-27 18:31:01 +0900

    Combine Operator map&filter

--- Combine/Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Combine/Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -4,9 +4,28 @@ import Foundation
 import Combine
 
 // Transform - Map
+let numPublisher = PassthroughSubject<Int,Never>()
+let subscriotin = numPublisher
+    .map { $0 * 2}
+    .sink { value in
+        print(value)
+    }
+numPublisher.send(10)
 
+    
 
 // Filter
+let stringPublisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPublisher
+    .filter { $0.contains("a") }
+    .sink { value in
+        print(value)
+    }
 
+stringPublisher.send("abc")
+stringPublisher.send("bbb")
+stringPublisher.send("woo")
+stringPublisher.send("bbba")
+subscription2.cancel()
 
 //: [Next](@next)
