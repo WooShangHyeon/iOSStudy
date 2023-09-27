32f78dc32c8642f80763b3b6c5922eb147e7b583
wooshanghyeon <woosh9912@naver.com>
2023-09-27 19:31:29 +0900

    Combine Operator removeDup & compactMap

--- Combine/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Combine/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -6,18 +6,43 @@ import Combine
 var subscriptions = Set<AnyCancellable>()
 
 // removeDuplicates
-
+let words = "woo shang hyeon go go naver !"
+    .components(separatedBy: " ")
+    .publisher
+words
+    .removeDuplicates()
+    .sink { value in
+        print(value)
+    }.store(in: &subscriptions)
 
 // compactMap
 
+let strings = ["a", "3.14", "11", "22", "woo", "4ws1"].publisher
+
+strings
+    .compactMap { Float($0)}
+    .sink { value in
+        print(value)
+    }.store(in: &subscriptions)
 
 
 // ignoreOutput
 
+let numbers = (1...10_000).publisher
+
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print($0) }, receiveValue: { print($0) } )
+    .store(in: &subscriptions)
 
 
 // prefix
+let tens = (1...10).publisher
 
+tens
+    .prefix(2)
+    .sink(receiveCompletion: { print($0) }, receiveValue: { print($0) } )
+    .store(in: &subscriptions)
 
 
 //: [Next](@next)
