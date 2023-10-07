74782d0610573e481adeec857b09757fe7ba864c
wooshanghyeon <woosh9912@naver.com>
2023-10-07 16:17:37 +0900

    SwiftUI Stack

--- SwiftUI/SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
+++ SwiftUI/SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ImageView()
+            ButtonView()
+            TextView()
+            Spacer()
+            HStack {
+                ImageView()
+                ButtonView()
+                TextView()
+            }
+            Spacer()
+            ZStack {
+                ImageView()
+                ButtonView()
+                TextView()
+            }
         }
-        .padding()
     }
 }
 
