765dd7d73ab84244900cdcb19a37ffe440dfa507
wooshanghyeon <woosh9912@naver.com>
2023-10-16 21:30:26 +0900

    CloneProject생성

---
+++ CloneProject/TossBenefitTab/TossBenefitTab/BenefitListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BenefitListViewController.swift
+//  TossBenefitTab
+//
+//  Created by 우상현 on 10/16/23.
+//
+
+import UIKit
+
+class BenefitListViewController: UIViewController {
+    
+    
+//    사용자 확인요소:
+//    1. 나의 포인트
+//    2. 오늘의 혜택
+//    3. 다른 혜택 리스트
+    
+//    셀 클릭시:
+//    1. 포인트 셀 클릭시 -> 상세뷰로 전환
+//    2. 혜택 셀 클릭시 -> 상세뷰로 전환
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "혜택"
+    }
+}
