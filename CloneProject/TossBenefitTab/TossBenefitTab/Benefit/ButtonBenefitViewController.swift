b16b707a247df97d5db7538fe7ec0ca7e6e60400
wooshanghyeon <woosh9912@naver.com>
2023-10-18 19:11:17 +0900

    benefit detail view에 코드뷰 넣음

--- CloneProject/TossBenefitTab/TossBenefitTab/Benefit/ButtonBenefitViewController.swift
+++ CloneProject/TossBenefitTab/TossBenefitTab/Benefit/ButtonBenefitViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ButtonBenefitViewController: UIViewController {
 
     @IBOutlet weak var ctaButton: UIButton!
+    @IBOutlet weak var vStackView: UIStackView!
+    
     
     var benefit: Benefit = .today
     var benefitDetails: BenefitDetails = .default
@@ -18,14 +20,31 @@ class ButtonBenefitViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ctaButton.layer.masksToBounds = true
-        ctaButton.layer.cornerRadius = 5
-    
-        navigationItem.largeTitleDisplayMode = .never
-        
+        setupUI()
+        addGuides()
         ctaButton.setTitle(benefit.ctaTitle, for: .normal)
         
     }
    
-
+    private func setupUI() {
+        ctaButton.layer.masksToBounds = true
+        ctaButton.layer.cornerRadius = 5
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    private func addGuides() {
+        let guideviews: [BenefitGuideView] = benefitDetails.guides.map { guide in
+            let guideView = BenefitGuideView(frame: .zero)
+            guideView.icon.image = UIImage(systemName: guide.iconName)
+            guideView.title.text = guide.guide
+            return guideView
+        }
+        
+        guideviews.forEach { view in
+            self.vStackView.addArrangedSubview(view)
+            NSLayoutConstraint.activate([
+                view.heightAnchor.constraint(equalToConstant: 60)
+            ])
+        }
+    }
 }
