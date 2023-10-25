f7ef677dacb4c5d686211677afc72b1e70570fd6
wooshanghyeon <woosh9912@naver.com>
2023-10-25 16:37:37 +0900

    viewController생성 및 화면 구조 설계 네비게이션->탭바->여러화면

---
+++ CloneProject/CarrotHomeTab/CarrotHomeTab/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  CarrotHomeTab
+//
+//  Created by 우상현 on 10/25/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
---
+++ CloneProject/CarrotHomeTab/CarrotHomeTab/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  CarrotHomeTab
+//
+//  Created by 우상현 on 10/25/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBAction func ctaButtonTapped(_ sender: Any) {
+        
+        let sb = UIStoryboard(name: "Detail", bundle: nil)
+        
+        let vc = sb.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
