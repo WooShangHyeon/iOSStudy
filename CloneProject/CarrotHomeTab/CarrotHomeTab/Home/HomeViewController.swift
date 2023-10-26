6022155cf9754972e5d50f71b57f1a5a4d0ecf1a
wooshanghyeon <woosh9912@naver.com>
2023-10-26 21:49:44 +0900

    Home View Controller

--- CloneProject/CarrotHomeTab/CarrotHomeTab/Home/HomeViewController.swift
+++ CloneProject/CarrotHomeTab/CarrotHomeTab/Home/HomeViewController.swift
@@ -6,25 +6,48 @@
 //
 
 import UIKit
+import Combine
 
 // 홈에 뷰모델을 만들기
 // 뷰모델이 리스트 가져오기
 
 class HomeViewController: UIViewController {
     
+    let viewModel: HomeViewModel = HomeViewModel(network: NetworkService(configuration: .default))
+    var subscriptions = Set<AnyCancellable>()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    
+        bind()
+        viewModel.fetch()
     }
     
-    @IBAction func ctaButtonTapped(_ sender: Any) {
+    private func bind() {
+        viewModel.$items
+            .receive(on: RunLoop.main)
+            .sink { items in
+                print("--> Update collection : \(items)")
+            }.store(in: &subscriptions)
         
-        let sb = UIStoryboard(name: "Detail", bundle: nil)
-        
-        let vc = sb.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        
-        navigationController?.pushViewController(vc, animated: true)
+        viewModel.itemTapped
+            .sink { items in
+                let sb = UIStoryboard(name: "Detail", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+                self.navigationController?.pushViewController(vc, animated: true)
+            }.store(in: &subscriptions)
     }
+    
+    
     
 }
+
+
+//@IBAction func ctaButtonTapped(_ sender: Any) {
+//    
+//    let sb = UIStoryboard(name: "Detail", bundle: nil)
+//    
+//    let vc = sb.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+//    
+//    navigationController?.pushViewController(vc, animated: true)
+//}
